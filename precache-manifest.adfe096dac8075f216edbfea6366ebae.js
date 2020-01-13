2092e520140372c6270a18d663b3f90949e78214
Sam Cross <crossmansam@gmail.com>
2020-01-13 21:12:32 +0000

    Updates

---
+++ precache-manifest.adfe096dac8075f216edbfea6366ebae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3898cd76dc360255dbce7bba4df9ce4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "282650cceb15e42966c7",
+    "url": "/static/css/main.4b9484bb.chunk.css"
+  },
+  {
+    "revision": "16e908254b731be58017",
+    "url": "/static/js/2.9f5429a5.chunk.js"
+  },
+  {
+    "revision": "1b23ba65f027fa0b538ecc6055ad39ba",
+    "url": "/static/js/2.9f5429a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "282650cceb15e42966c7",
+    "url": "/static/js/main.19e38a9b.chunk.js"
+  },
+  {
+    "revision": "aeabd0c7df37d8e6c3c8",
+    "url": "/static/js/runtime-main.2f651627.js"
+  },
+  {
+    "revision": "41f4d654ed3cf990a5701d28ccd8ea4c",
+    "url": "/static/media/aboutData.41f4d654.md"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
