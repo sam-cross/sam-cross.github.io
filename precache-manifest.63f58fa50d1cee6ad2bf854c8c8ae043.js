bc511d41f3345b289c386e2d331a88edee66d76c
Sam Cross <crossmansam@gmail.com>
2020-01-13 12:53:51 +0000

    Updates

--- precache-manifest.63f58fa50d1cee6ad2bf854c8c8ae043.js
+++ precache-manifest.63f58fa50d1cee6ad2bf854c8c8ae043.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f2bb537c8698bd9a427c6ed5f1b0b519",
+    "revision": "ce59bca9f50b5dd2a069dfdf5d9e6628",
     "url": "/index.html"
   },
   {
-    "revision": "25735eec12be6f8dcf5e",
+    "revision": "737873fd7124d78f693a",
     "url": "/static/css/main.4b9484bb.chunk.css"
   },
   {
-    "revision": "a4538e27a04ca8004994",
-    "url": "/static/js/2.1e081daf.chunk.js"
+    "revision": "c45dbcdd63a42fdeadb5",
+    "url": "/static/js/2.b545f80b.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/static/js/2.1e081daf.chunk.js.LICENSE"
+    "url": "/static/js/2.b545f80b.chunk.js.LICENSE"
   },
   {
-    "revision": "25735eec12be6f8dcf5e",
-    "url": "/static/js/main.2a9afe35.chunk.js"
+    "revision": "737873fd7124d78f693a",
+    "url": "/static/js/main.f87d7a42.chunk.js"
   },
   {
     "revision": "aeabd0c7df37d8e6c3c8",
